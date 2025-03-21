6af6e66fb1f9a6c785a520ae444681984caa0e0a
sam-beaton <sambeaton1710@gmail.com>
2025-03-21 19:15:14 +0000

    Fixed matrix size compatibility issue when assessing HbR channels

--- pruningComparisonsMatlab/+pruneTools/calculateChannelSNR.m
+++ pruningComparisonsMatlab/+pruneTools/calculateChannelSNR.m
@@ -6,11 +6,17 @@ function snrMat = calculateChannelSNR(nirs, chanIdx, windowInfo, snrMat, nsub, r
 
     % Extract channel data
     chanData = nirs.d(:, chanIdx);
+
+    if chanIdx > size(windowInfo.windowMotion,1)
+        chanIdxMotion = chanIdx - size(windowInfo.windowMotion,1);
+    else
+        chanIdxMotion = chanIdx;
+    end
     
     % Process each window
     for iWindow = 1:windowInfo.windowCount
         % Remove non-valid (motion) samples from SNR calculation
-        if windowInfo.windowMotion(chanIdx, iWindow) == 0
+        if windowInfo.windowMotion(chanIdxMotion, iWindow) == 0
             if params.pruneQT == 1
                 windowStart = find(nirs.t == windowInfo.windowTimes(1, iWindow));
                 windowEnd = find(nirs.t == windowInfo.windowTimes(2, iWindow));
